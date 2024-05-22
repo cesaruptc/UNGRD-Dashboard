f0852948568c6a1810cd4af7adc622af47adb02d
Carlos <73614255+Pidual@users.noreply.github.com>
2024-05-22 17:50:33 -0500

    diabloo

--- ACTIVIDAD 3/FRONT-END/frontend/src/App.js
+++ ACTIVIDAD 3/FRONT-END/frontend/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import MyChart from './components/MyChart';
 import Top5Events from './components/Top5Events';
+import Top5EventsChart from './components/Top5EventsChart';
+import './index.css'; 
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <MyChart/>
-      <Top5Events/>
+      <div class="flex flex-row">
+        <Top5Events/>
+        <Top5EventsChart/>
+      </div>
+      
     </div>
   );
 }
