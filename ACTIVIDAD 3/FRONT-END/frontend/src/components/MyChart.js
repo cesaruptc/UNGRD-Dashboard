5f1ef2c8a54937d29c5db05003aa8b7cb95761d7
Carlos <73614255+Pidual@users.noreply.github.com>
2024-05-22 16:21:55 -0500

    Chart Software ready fellz

--- ACTIVIDAD 3/FRONT-END/frontend/src/components/MyChart.js
+++ ACTIVIDAD 3/FRONT-END/frontend/src/components/MyChart.js
@@ -1,6 +1,27 @@
 // src/components/MyChart.js
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+// Registering components
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const MyChart = () => {
     const data = {
